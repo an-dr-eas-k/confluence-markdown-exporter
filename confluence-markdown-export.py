3451e39271733dc1f4feb1c7e1bbe61298995f8a
Andreas Kirschner <kirscha5@emea.roche.com>
2024-03-11 15:18:59 +0100

    refactor, be even more general

--- confluence-markdown-export.py
+++ confluence-markdown-export.py
@@ -24,24 +24,24 @@ class PageMetadata:
     page_id: str
     child_ids: list
     content: str
-    extension = ".html"
     document_name: str
     sanitized_filename: str
-    sanitized_parent: str
+    sanitized_parents: str
     page_location: str
     page_filename: str
     page_output_dir: str
 
-class ConfluenceConnection:
+class ConfluenceWorker:
     def __init__(self, url, token, out_dir, space):
         self.__out_dir = out_dir
-        self.__parsed_url = urlparse(url)
-        self.__token = token
-        self.__confluence = Confluence(url=urlunparse(self.__parsed_url), token=self.__token)
+        self.parsed_url = urlparse(url)
+        self.token = token
         self.__seen = set()
-        self.__space = space
 
-    def __sanitize_filename(self, document_name_raw):
+        self.confluence = Confluence(url=urlunparse(self.parsed_url), token=self.token)
+        self.space = space
+
+    def _sanitize_filename(self, document_name_raw):
         document_name = document_name_raw
         for invalid in ["..", "/", ">", "<", ":", "\"", "|", "?", "*", "\\"]:
             if invalid in document_name:
@@ -49,14 +49,21 @@ class ConfluenceConnection:
                 document_name = document_name.replace(invalid, "_")
         return document_name
 
-    def __obtain_page_metadata(self, page, parents) -> PageMetadata:
+    def _get_page(self, src_id):
+        if src_id in self.__seen:
+            # this could theoretically happen if Page IDs are not unique or there is a circle
+            raise ExportException("Duplicate Page ID Found!")
+
+        return self.confluence.get_page_by_id(src_id, expand="body.storage")
+
+    def _obtain_page_metadata(self, page, parents) -> PageMetadata:
         page_title = page["title"]
         page_id = page["id"]
     
         # see if there are any children
         child_ids = []
         try:
-            child_ids = self.__confluence.get_child_id_list(page_id)
+            child_ids = self.confluence.get_child_id_list(page_id)
         except Exception as _:
             logging.error ("Error getting child ids for page %s", page_id)
     
@@ -70,103 +77,112 @@ class ConfluenceConnection:
             document_name = page_title
 
         # make some rudimentary checks, to prevent trivial errors
-        sanitized_filename = self.__sanitize_filename(document_name) + extension
-        sanitized_parents = list(map(self.__sanitize_filename, parents))
+        sanitized_filename = self._sanitize_filename(document_name) + extension
+        sanitized_parents = list(map(self._sanitize_filename, parents))
 
         page_location = sanitized_parents + [sanitized_filename]
         page_filename = os.path.join(self.__out_dir, *page_location)
 
         page_output_dir = os.path.dirname(page_filename)
-        return PageMetadata(page_title, page_id, child_ids, content, extension, document_name, sanitized_filename, sanitized_parents, page_location, page_filename, page_output_dir)
 
-class Exporter(ConfluenceConnection):
+        self.__seen.add(page_id)
+
+        return PageMetadata(page_title, page_id, child_ids, content, document_name, sanitized_filename, 
+                            sanitized_parents, page_location, page_filename, page_output_dir)
+
+    def page_action(self, page_meta_data: PageMetadata):
+        pass
+
+    def _handle_page(self, src_id, parents):
+        page = self._get_page(src_id)
+        page_meta_data: PageMetadata = self._obtain_page_metadata(page, parents)
+
+        self.page_action(page_meta_data)
+    
+        # recurse to process child nodes
+        for child_id in page_meta_data.child_ids:
+            self._handle_page(child_id, parents=page_meta_data.sanitized_parents + [page_meta_data.page_title])
+
+    def _handle_space(self, space):
+        space_key = space["key"]
+        logging.info("Processing space %s", space_key)
+        if space.get("homepage") is None:
+            logging.error(
+                "Skipping space: %s, no homepage found!\nIn order for this tool to work there has to be a root page!", 
+                space_key)
+            raise ExportException("No homepage found")
+        else:
+            # homepage found, recurse from there
+            homepage_id = space["homepage"]["id"]
+            self._handle_page(homepage_id, parents=[space_key])
+
+    
+    def handle_instance(self):
+        start = 900 # todo: revert
+        limit = 50
+        
+        while True:
+            logging.debug ("start %s", start)
+            ret = self.confluence.get_all_spaces(start=start, limit=limit, expand='description.plain,homepage')
+            if ret['size'] <= 0:
+                break
+            for space in ret["results"]:
+                if self.space is not None and space["key"] == self.space:
+                    self._handle_space(space)
+                    return
+                if self.space is None:
+                    self._handle_space(space)
+            start += limit
+
+class Exporter(ConfluenceWorker):
     def __init__(self, url, token, out_dir, space, no_attach):
         super().__init__(url, token, out_dir, space)
         self.__no_attach = no_attach
 
 
-    def __dump_page(self, src_id, parents):
-        if src_id in self.__seen:
-            # this could theoretically happen if Page IDs are not unique or there is a circle
-            raise ExportException("Duplicate Page ID Found!")
+    def __handle_attachment(self, att_title, download, page_meta_data: PageMetadata):
+        ret = self.confluence.get_attachments_from_content(page_meta_data.page_id, start=0, limit=500, expand=None,
+                                                                filename=None, media_type=None)
+        for i in ret["results"]:
+            att_title = i["title"]
+            download = i["_links"]["download"]
 
-        page = self.__confluence.get_page_by_id(src_id, expand="body.storage")
-        page_meta_data: PageMetadata = super().__obtain_page_metadata(page, parents)
+            prefix = self.parsed_url.path
+            att_url = urlunparse(
+                (self.parsed_url[0], self.parsed_url[1], prefix + download.lstrip("/"), None, None, None)
+            )
+            att_sanitized_name = self._sanitize_filename(att_title)
+            att_filename = os.path.join(page_meta_data.page_output_dir, ATTACHMENT_FOLDER_NAME, att_sanitized_name)
+
+            att_dirname = os.path.dirname(att_filename)
+            os.makedirs(att_dirname, exist_ok=True)
+
+            logging.debug("Saving attachment %s to %s", att_title, page_meta_data.page_location)
+
+            r = requests.get(att_url, headers={"Authorization": f"Bearer {self.token}"}, stream=True)
+            if 400 <= r.status_code:
+                if r.status_code == 404:
+                    logging.warning("Attachment %s not found (404)!", att_url)
+                    continue
+
+                # this is a real error, raise it
+                r.raise_for_status()
+
+            with open(att_filename, "wb") as f:
+                for buf in r.iter_content(chunk_size=DOWNLOAD_CHUNK_SIZE):
+                    f.write(buf)
+
+    def page_action(self, page_meta_data: PageMetadata):
+        super().page_action(page_meta_data)
 
         os.makedirs(page_meta_data.page_output_dir, exist_ok=True)
-        print("Saving to {}".format(" / ".join(page_meta_data.page_location)))
+        logging.debug("Saving to %s", "/".join(page_meta_data.page_location))
         with open(page_meta_data.page_filename, "w", encoding="utf-8") as f:
             f.write(page_meta_data.content)
 
         # fetch attachments unless disabled
         if not self.__no_attach:
-            ret = self.__confluence.get_attachments_from_content(page_meta_data.page_id, start=0, limit=500, expand=None,
-                                                                 filename=None, media_type=None)
-            for i in ret["results"]:
-                att_title = i["title"]
-                download = i["_links"]["download"]
-
-                prefix = self.__parsed_url.path
-                att_url = urlunparse(
-                    (self.__parsed_url[0], self.__parsed_url[1], prefix + download.lstrip("/"), None, None, None)
-                )
-                att_sanitized_name = self.__sanitize_filename(att_title)
-                att_filename = os.path.join(page_meta_data.page_output_dir, ATTACHMENT_FOLDER_NAME, att_sanitized_name)
-
-                att_dirname = os.path.dirname(att_filename)
-                os.makedirs(att_dirname, exist_ok=True)
-
-            logging.debug("Saving attachment %s to %s", att_title, page_meta_data.page_location)
-
-                r = requests.get(att_url, headers={"Authorization": f"Bearer {self.__token}"}, stream=True)
-                if 400 <= r.status_code:
-                    if r.status_code == 404:
-                    logging.warning("Attachment %s not found (404)!", att_url)
-                        continue
-
-                    # this is a real error, raise it
-                    r.raise_for_status()
-
-                with open(att_filename, "wb") as f:
-                    for buf in r.iter_content(chunk_size=DOWNLOAD_CHUNK_SIZE):
-                        f.write(buf)
-
-        self.__seen.add(page_meta_data.page_id)
-    
-        # recurse to process child nodes
-        for child_id in page_meta_data.child_ids:
-            self.__dump_page(child_id, parents=page_meta_data.sanitized_parents + [page_meta_data.page_title])
-
-    def __dump_space(self, space):
-        space_key = space["key"]
-        print("Processing space", space_key)
-        if space.get("homepage") is None:
-            print("Skipping space: {}, no homepage found!".format(space_key))
-            print("In order for this tool to work there has to be a root page!")
-            raise ExportException("No homepage found")
-        else:
-            # homepage found, recurse from there
-            homepage_id = space["homepage"]["id"]
-            self.__dump_page(homepage_id, parents=[space_key])
-
-    
-    def dump(self):
-        start = 0
-        limit = 50
-        
-        while True:
-            print ("start %s" % start)
-            ret = self.__confluence.get_all_spaces(start=start, limit=limit, expand='description.plain,homepage')
-            if ret['size'] <= 0:
-                break
-            for space in ret["results"]:
-                print ("processing space %s" % space["key"])
-                if self.__space is not None and space["key"] == self.__space:
-                    self.__dump_space(space)
-                    return
-                if self.__space is None:
-                    self.__dump_space(space)
-            start += limit
+            self.__handle_attachment(None, None, page_meta_data)
 
 
 
@@ -248,9 +264,9 @@ if __name__ == "__main__":
             return
 
         if not args.no_fetch:
-            dumper = Exporter(url=args.url, token=args.token, out_dir=args.out_dir,
+            exporter = Exporter(url=args.url, token=args.token, out_dir=args.out_dir,
                             space=args.space, no_attach=args.no_attach)
-            dumper.dump()
+            exporter.handle_instance()
             
         converter = Converter(out_dir=args.out_dir)
         converter.convert()
